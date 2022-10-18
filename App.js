0a8e628942f457fc6f51d40e13541ff61bae9d31
Cem Mandraci <cemmandraci@hotmail.com>
2022-10-18 16:56:53 +0300

    #Upload dynamic data

--- App.js
+++ App.js
@@ -1,18 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+import * as Location from 'expo-location';
 
 import DateTime from './components/DateTime';
 import WeatherScroll from './components/WeatherScroll';
 
+const API_KEY = '35423bce4a9e7703e9020add7bbbd7b8';
+
 const img = require('./assets/weather.jpg')
 
 export default function App() {
+
+  const [data, setData] = useState({})
+
+  useEffect(() => {
+    (async () => {
+      
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+          fetchDataFromApi("40.7128","-74.0060")
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      fetchDataFromApi(location.coords.latitude, location.coords.longitude);
+    })();
+  }, [])
+
+
+  const fetchDataFromApi = (latitude, longitude) => {
+    if(latitude && longitude) {
+        fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&units=metric&appid=${API_KEY}`).then(res => res.json()).then(data => {
+
+        setData(data)
+      })
+    }
+    
+  }
+
+
   return (
     <View style={styles.container}>
       <ImageBackground source={img} style={styles.image}>
-          <DateTime />
-          <WeatherScroll />
+          <DateTime current={data.current} timezone={data.timezone} lat={data.lat} lon={data.lon} />
+          <WeatherScroll weatherData={data.daily} />
       </ImageBackground>
     </View>
   );
--- components/DateTime.js
+++ components/DateTime.js
@@ -1,5 +1,9 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import moment from 'moment-timezone';
+
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
 
 
@@ -14,27 +18,49 @@ const WeatherItem = ({title, value, unit}) => {
 }
 
 
-const DateTime = () => {
+const DateTime = ({current, lat, lon, timezone}) => {
+
+    const[date,setDate] = useState('')
+    const[time,setTime] = useState('')
+
+    useEffect (() => {
+        setInterval(() => {
+            const time = new Date();
+            const month = time.getMonth();
+            const date = time.getDate();
+            const day = time.getDay();
+            const hour = time.getHours()
+            const hoursIn12HrFormat = hour >= 13 ? hour %12: hour
+            const minutes = time.getMinutes();
+            const ampm = hour >= 12 ? "PM" : "AM"
+
+           setTime((hoursIn12HrFormat < 10? '0' + hoursIn12HrFormat : hoursIn12HrFormat) + ':' + (minutes < 10? '0' + minutes : minutes) + ' ' + ampm)
+
+            setDate(days[day] + ', ' + date + ' ' + months[month])    
+            
+        }, 1000);
+    }, [])
+
 
     return (
         <View style={styles.container}>
             <View>
                 <View>
-                    <Text style={styles.heading}>12:30am</Text>
+                    <Text style={styles.heading}>{time}</Text>
                 </View>
                 <View>
-                    <Text style={styles.subHeading}>Monday,June</Text>
+                    <Text style={styles.subHeading}>{date}</Text>
                 </View>
                 <View style={styles.weatherItemContainer}>
-                    <WeatherItem title="Humidity" value="79" unit="%" />
-                    <WeatherItem title="Pressure" value="1000" unit="hPa" />
-                    <WeatherItem title="Sunrise" value="06:00" unit="am" />
-                    <WeatherItem title="Sunset" value="06:30" unit="pm" />
+                    <WeatherItem title="Humidity" value={current? current.humidity : ''} unit="%" />
+                    <WeatherItem title="Pressure" value={current? current.pressure : ''} unit="hPa" />
+                    <WeatherItem title="Sunrise" value={current? moment.tz(current.sunrise * 1000, timezone).format('HH:mm') : ''} unit="am" />
+                    <WeatherItem title="Sunset" value={current? moment.tz(current.sunset * 1000, timezone).format('HH:mm') : ''} unit="pm" />
                 </View>
             </View>
             <View style={styles.rightAlign}>
-                <Text style={styles.timezone}>Burgaz/Turkey</Text>
-                <Text style={styles.latlong}>3.80N 48E</Text>
+                <Text style={styles.timezone}>{timezone}</Text>
+                <Text style={styles.latlong}>{lat}N {lon}E</Text>
             </View>
         </View>
     )
